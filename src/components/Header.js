2577c92208463a7737a0296b2cf788eadc302257
Hammad Hassan <hammadhassan616@gmail.com>
2023-01-06 08:42:15 +0100

    added css and header

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+ import '../App.css';
+const Header = props => {
+
+    /* Header */
+  return (
+    <header class="header">
+
+    <a href="#home" class="logo"> <img src="images/logo1.png" alt=""/> </a>
+
+    <form action="" class="search-form">
+        <input type="search" name="" placeholder="search here..." id="search-box"/>
+        <label for="search-box" class="fas fa-search"></label>
+    </form>
+
+    <div class="icons">
+        <div id="menu-btn" class="fas fa-bars"></div>
+        <div id="search-btn" class="fas fa-search"></div>
+        <a href="#" class="fas fa-heart"></a>
+        <a href="#" class="fas fa-shopping-cart"></a>
+    </div>
+
+</header>
+  
+)
+
+}
+
+
+export default Header
