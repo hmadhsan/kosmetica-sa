abfaba5badab0e6c71bba5f25a823aa8d42b1bfc
Hammad Hassan <hammadhassan616@gmail.com>
2023-01-06 09:30:58 +0100

    category section

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Categories from './components/Categories';
 function App() {
   return (
     <div className="App">
     <Header />
     <Navbar />
     <Home />
-     
+    <Categories />
     </div>
   );
 }
---
+++ src/components/Categories.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import '../App.css';
+const Categories = () => {
+    /* category section starts */
+  return (
+    <section class="category">
+
+    <a href="#" class="box">
+    <img src={require('../images/category-1.png')} alt=""/>
+        <p>cosmetics</p>
+    </a>
+
+    <a href="#" class="box">
+    <img src={require('../images/category-2.png')} alt=""/>
+        <p>makeup</p>
+    </a>
+
+    <a href="#" class="box">
+        <img src={require('../images/category-3.png')} alt="" />
+        <p>powder</p>
+    </a>
+
+    <a href="#" class="box">
+    <img src={require('../images/category-4.png')} alt=""/>
+        <p>lotions</p>
+    </a>
+
+    <a href="#" class="box">
+    <img src={require('../images/category-5.png')} alt=""/>
+        <p>lipstick</p>
+    </a>
+
+    <a href="#" class="box">
+    <img src={require('../images/category-6.png')} alt=""/>
+        <p>mascara</p>
+    </a>
+
+</section>
+  )
+}
+
+export default Categories
