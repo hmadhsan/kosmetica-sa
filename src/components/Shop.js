0145ee37c93d4ef39c8ced2d31f48b9bdf7019d5
Hammad Hassan <hammadhassan616@gmail.com>
2023-01-15 21:37:54 +0100

    update shop.js

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -27,25 +27,25 @@ useEffect(() => {
 },[])
     /* shop section starts */
   return (
-<section class="shop" id="shop">
+<section className="shop" id="shop">
 
-<div class="heading">
+<div className="heading">
     <h1>featured products</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi consequuntur officia beatae distinctio
         minus optio?</p>
 </div>
 
-<div class="swiper products-slider">
+<div className="swiper products-slider">
 
-    <div class="swiper-wrapper">
+    <div className="swiper-wrapper">
 
-        <div class="swiper-slide slide">
-            <div class="image">
-                <span class="discount">-25%</span>
+        <div className="swiper-slide slide">
+            <div className="row">
+                <span className="discount">-25%</span>
                   
                 {products.map(item => {
                     return (
-                        <div>
+                        <div className='col-md-3'>
                             <img src={item.image} alt='hello' />
                             <h1> {item.name} </h1>
                             </div>
@@ -55,190 +55,190 @@ useEffect(() => {
                 <h1> {products.length} </h1>
 
               
-                <div class="icons">
-                    <a href="#" class="fas fa-shopping-cart"></a>
-                    <a href="#" class="fas fa-heart"></a>
-                    <a href="#" class="fas fa-share"></a>
+                <div className="icons">
+                    <a href="#" className="fas fa-shopping-cart"></a>
+                    <a href="#" className="fas fa-heart"></a>
+                    <a href="#" className="fas fa-share"></a>
                 </div>
             </div>
-            <div class="content">
-                <div class="stars">
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star-half-alt"></i>
+            <div className="content">
+                <div className="stars">
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star-half-alt"></i>
                 </div>
                 <p>beauty products</p>
-                <div class="price">$49.99 <span>$79.99</span></div>
+                <div className="price">$49.99 <span>$79.99</span></div>
             </div>
         </div>
 
-        <div class="swiper-slide slide">
-            <div class="image">
-                <span class="discount">-25%</span>
+        <div className="swiper-slide slide">
+            <div className="image">
+                <span className="discount">-25%</span>
                 <img src={require('../images/product-2.jpg')} alt=""/>
-                <div class="icons">
-                    <a href="#" class="fas fa-shopping-cart"></a>
-                    <a href="#" class="fas fa-heart"></a>
-                    <a href="#" class="fas fa-share"></a>
+                <div className="icons">
+                    <a href="#" className="fas fa-shopping-cart"></a>
+                    <a href="#" className="fas fa-heart"></a>
+                    <a href="#" className="fas fa-share"></a>
                 </div>
             </div>
-            <div class="content">
-                <div class="stars">
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star-half-alt"></i>
+            <div className="content">
+                <div className="stars">
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star-half-alt"></i>
                 </div>
                 <p>beauty products</p>
-                <div class="price">$49.99 <span>$79.99</span></div>
+                <div className="price">$49.99 <span>$79.99</span></div>
             </div>
         </div>
 
-        <div class="swiper-slide slide">
-            <div class="image">
-                <span class="discount">-25%</span>
+        <div className="swiper-slide slide">
+            <div className="image">
+                <span className="discount">-25%</span>
                 <img src={require('../images/product-3.jpg')} alt=""/>
-                <div class="icons">
-                    <a href="#" class="fas fa-shopping-cart"></a>
-                    <a href="#" class="fas fa-heart"></a>
-                    <a href="#" class="fas fa-share"></a>
+                <div className="icons">
+                    <a href="#" className="fas fa-shopping-cart"></a>
+                    <a href="#" className="fas fa-heart"></a>
+                    <a href="#" className="fas fa-share"></a>
                 </div>
             </div>
-            <div class="content">
-                <div class="stars">
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star-half-alt"></i>
+            <div className="content">
+                <div className="stars">
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star-half-alt"></i>
                 </div>
                 <p>beauty products</p>
-                <div class="price">$49.99 <span>$79.99</span></div>
+                <div className="price">$49.99 <span>$79.99</span></div>
             </div>
         </div>
 
-        <div class="swiper-slide slide">
-            <div class="image">
-                <span class="discount">-25%</span>
+        <div className="swiper-slide slide">
+            <div className="image">
+                <span className="discount">-25%</span>
                 <img src={require('../images/product-4.jpg')} alt=""/>
-                <div class="icons">
-                    <a href="#" class="fas fa-shopping-cart"></a>
-                    <a href="#" class="fas fa-heart"></a>
-                    <a href="#" class="fas fa-share"></a>
+                <div className="icons">
+                    <a href="#" className="fas fa-shopping-cart"></a>
+                    <a href="#" className="fas fa-heart"></a>
+                    <a href="#" className="fas fa-share"></a>
                 </div>
             </div>
-            <div class="content">
-                <div class="stars">
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star-half-alt"></i>
+            <div className="content">
+                <div className="stars">
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star-half-alt"></i>
                 </div>
                 <p>beauty products</p>
-                <div class="price">$49.99 <span>$79.99</span></div>
+                <div className="price">$49.99 <span>$79.99</span></div>
             </div>
         </div>
 
-        <div class="swiper-slide slide">
-            <div class="image">
-                <span class="discount">-25%</span>
+        <div className="swiper-slide slide">
+            <div className="image">
+                <span className="discount">-25%</span>
                 <img src={require('../images/product-5.jpg')} alt=""/>
-                <div class="icons">
-                    <a href="#" class="fas fa-shopping-cart"></a>
-                    <a href="#" class="fas fa-heart"></a>
-                    <a href="#" class="fas fa-share"></a>
+                <div className="icons">
+                    <a href="#" className="fas fa-shopping-cart"></a>
+                    <a href="#" className="fas fa-heart"></a>
+                    <a href="#" className="fas fa-share"></a>
                 </div>
             </div>
-            <div class="content">
-                <div class="stars">
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star-half-alt"></i>
+            <div className="content">
+                <div className="stars">
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star-half-alt"></i>
                 </div>
                 <p>beauty products</p>
-                <div class="price">$49.99 <span>$79.99</span></div>
+                <div className="price">$49.99 <span>$79.99</span></div>
             </div>
         </div>
 
-        <div class="swiper-slide slide">
-            <div class="image">
-                <span class="discount">-25%</span>
+        <div className="swiper-slide slide">
+            <div className="image">
+                <span className="discount">-25%</span>
                 <img src={require('../images/product-6.jpg')} alt=""/>
-                <div class="icons">
-                    <a href="#" class="fas fa-shopping-cart"></a>
-                    <a href="#" class="fas fa-heart"></a>
-                    <a href="#" class="fas fa-share"></a>
+                <div className="icons">
+                    <a href="#" className="fas fa-shopping-cart"></a>
+                    <a href="#" className="fas fa-heart"></a>
+                    <a href="#" className="fas fa-share"></a>
                 </div>
             </div>
-            <div class="content">
-                <div class="stars">
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star-half-alt"></i>
+            <div className="content">
+                <div className="stars">
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star-half-alt"></i>
                 </div>
                 <p>beauty products</p>
-                <div class="price">$49.99 <span>$79.99</span></div>
+                <div className="price">$49.99 <span>$79.99</span></div>
             </div>
         </div>
 
-        <div class="swiper-slide slide">
-            <div class="image">
-                <span class="discount">-25%</span>
+        <div className="swiper-slide slide">
+            <div className="image">
+                <span className="discount">-25%</span>
                 <img src={require('../images/product-7.jpg')} alt=""/>
-                <div class="icons">
-                    <a href="#" class="fas fa-shopping-cart"></a>
-                    <a href="#" class="fas fa-heart"></a>
-                    <a href="#" class="fas fa-share"></a>
+                <div className="icons">
+                    <a href="#" className="fas fa-shopping-cart"></a>
+                    <a href="#" className="fas fa-heart"></a>
+                    <a href="#" className="fas fa-share"></a>
                 </div>
             </div>
-            <div class="content">
-                <div class="stars">
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star-half-alt"></i>
+            <div className="content">
+                <div className="stars">
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star-half-alt"></i>
                 </div>
                 <p>beauty products</p>
-                <div class="price">$49.99 <span>$79.99</span></div>
+                <div className="price">$49.99 <span>$79.99</span></div>
             </div>
         </div>
 
-        <div class="swiper-slide slide">
-            <div class="image">
-                <span class="discount">-25%</span>
+        <div className="swiper-slide slide">
+            <div className="image">
+                <span className="discount">-25%</span>
                 <img src={require('../images/product-8.jpg')} alt=""/>
-                <div class="icons">
-                    <a href="#" class="fas fa-shopping-cart"></a>
-                    <a href="#" class="fas fa-heart"></a>
-                    <a href="#" class="fas fa-share"></a>
+                <div className="icons">
+                    <a href="#" className="fas fa-shopping-cart"></a>
+                    <a href="#" className="fas fa-heart"></a>
+                    <a href="#" className="fas fa-share"></a>
                 </div>
             </div>
-            <div class="content">
-                <div class="stars">
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star-half-alt"></i>
+            <div className="content">
+                <div className="stars">
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star-half-alt"></i>
                 </div>
                 <p>beauty products</p>
-                <div class="price">$49.99 <span>$79.99</span></div>
+                <div className="price">$49.99 <span>$79.99</span></div>
             </div>
         </div>
 
     </div>
 
-    <div class="swiper-button-next"></div>
-    <div class="swiper-button-prev"></div>
+    <div className="swiper-button-next"></div>
+    <div className="swiper-button-prev"></div>
 
 </div>
 
