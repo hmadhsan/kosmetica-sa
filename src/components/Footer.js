c9d2d8618279ce4251291e2eaac004cb62c22581
Hammad Hassan <hammadhassan616@gmail.com>
2023-01-30 20:59:35 +0100

    updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -22,7 +22,7 @@ const Footer = () => {
             <a href="#" className="link"> <i className="fas fa-angle-right"></i> My order </a>
             <a href="#" className="link"> <i className="fas fa-angle-right"></i> My favorite </a>
             <a href="#" className="link"> <i className="fas fa-angle-right"></i> My wishlist </a>
-            <a href="#" className="link"> <i className="fas fa-angle-right"></i> private policy </a>
+            <a href="#" className="link"> <i className="fas fa-angle-right"></i> Private policy </a>
             <a href="#" className="link"> <i className="fas fa-angle-right"></i> terms of use </a>
         </div>
 
