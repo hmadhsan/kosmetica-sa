3a51aa711beba6a544b3003985b9f6796b844bcf
Hammad Hassan <hammadhassan616@gmail.com>
2023-01-31 11:12:23 +0100

    updated

--- src/_reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import customerReducer from './customerReducer';
-
-const rootReducer = combineReducers({
-  customer: customerReducer,
-});
-
-export default rootReducer;
