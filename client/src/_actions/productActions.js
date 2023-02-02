f3e147874b23c3d653fbd8a2150d3eb6f43c0be2
Hammad Hassan <hammadhassan616@gmail.com>
2023-02-02 09:52:37 +0100

    category_list in prod actions

--- client/src/_actions/productActions.js
+++ client/src/_actions/productActions.js
@@ -1,10 +1,29 @@
 import axios from "axios";
 import { useDispatch } from "react-redux";
-import { PRODUCT_LIST, PRODUCT_LIST_MORE } from "./types";
+import { PRODUCT_LIST, PRODUCT_LIST_MORE, CATEGORY_LIST } from "./types";
 
 const useProducts = () => {
     const dispatch = useDispatch();
     const getProductList = (query) => {
+        const result = axios.post("/products", query).then((res) => {
+            return res.data
+        }).catch((err) => {
+            return err.response.data
+        })
+        if (query.loadMore) {
+            dispatch({
+                type: PRODUCT_LIST_MORE,
+                payload: result
+            });
+        } else {
+            dispatch({
+                type: PRODUCT_LIST,
+                payload: result
+            });
+        }
+
+    };
+    const getCategoryList = (query) => {
         const result = axios.post("/products", query).then((res) => {
             return res.data
         }).catch((err) => {
