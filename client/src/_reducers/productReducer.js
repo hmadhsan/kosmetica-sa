ba51acb170e628601f5b10a56a2691ebba90245e
Hammad Hassan <hammadhassan616@gmail.com>
2023-02-02 09:57:12 +0100

    updated categoryList

--- client/src/_reducers/productReducer.js
+++ client/src/_reducers/productReducer.js
@@ -1,24 +1,30 @@
-import { PRODUCT_LIST, PRODUCT_LIST_MORE } from "../_actions/types";
+import { PRODUCT_LIST, PRODUCT_LIST_MORE, CATEGORY_LIST } from "../_actions/types";
 
 const initialState = {
-    productList: null
+    productList: null,
+    categoryList: null
 }
 
 const productReducer = (state = initialState, action) => {
     switch (action.type) {
+        case CATEGORY_LIST:
+            return {
+                ...state,
+                categoryList: action.payload.data
+            }
         case PRODUCT_LIST:
             return {
                 ...state,
                 productList: action.payload.data
             }
-            case PRODUCT_LIST_MORE:
-                return {
-                    ...state,
-                    productList:[...state.productList, ...action.payload.data]
-                }
+        case PRODUCT_LIST_MORE:
+            return {
+                ...state,
+                productList: [...state.productList, ...action.payload.data]
+            }
 
         default:
-        return state;
+            return state;
     }
 }
 
