f6a2893684d3f3b1ed1b9312e5b04bc148868045
Hammad Hassan <hammadhassan616@gmail.com>
2023-02-09 10:41:42 +0100

    Update OrderHistory.js

--- client/src/containers/OrderHistory.js
+++ client/src/containers/OrderHistory.js
@@ -1,4 +1,4 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { PageHeader, Table } from 'antd';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
@@ -6,18 +6,29 @@ import useOrders from '../_actions/orderActions';
 import moment from 'moment'
 const OrderHistory = () => {
     const navigate = useNavigate();
-    const {getOrderHistory} = useOrders();
-   const orderHistory = useSelector(state => state.order?.orderHistory);
+    const { getOrderHistory } = useOrders();
+    const orderHistory = useSelector(state => state.order?.orderHistory);
     const renderOrderList = () => {
         return (
             <Table columns={columns} dataSource={orderHistory} />
         )
     }
 
+    const columns = [
+        { title: 'id', key: '_id', dataIndex: '_id' },
+        {
+            title: 'Date', key: 'orderDate', dataIndex: 'orderDate', align: 'center',
+            render: (value) => {
+                const dateFormat = moment(value).format('DD-MM-YYYY');
+                return dateFormat;
+            },
+        },
+        { title: 'Total Order ($)', key: 'totalAmount', dataIndex: 'totalAmount', align: 'right' }
+    ]
+
     return (
         <>
             <PageHeader title='Your Order History' onBack={() => navigate(-1)} />
-
         </>
     )
 }
