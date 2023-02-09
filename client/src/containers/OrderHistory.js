e5255d6347deef35967dc718206d7b7792b3898e
Hammad Hassan <hammadhassan616@gmail.com>
2023-02-09 10:31:54 +0100

    order history

--- client/src/containers/OrderHistory.js
+++ client/src/containers/OrderHistory.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import { PageHeader } from 'antd';
+import React,{useEffect} from 'react';
+import { PageHeader, Table } from 'antd';
 import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import useOrders from '../_actions/orderActions';
+import moment from 'moment'
 const OrderHistory = () => {
     const navigate = useNavigate();
+    const {getOrderHistory} = useOrders();
+   const orderHistory = useSelector(state => state.order?.orderHistory);
+    const renderOrderList = () => {
+        return (
+            <Table columns={columns} dataSource={orderHistory} />
+        )
+    }
+
     return (
         <>
             <PageHeader title='Your Order History' onBack={() => navigate(-1)} />
